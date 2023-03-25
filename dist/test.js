36e7f081324e5bc2de4cfcac82ecc146201f367b
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-26 00:30:19 +0900

    [Test] 타입 확인

--- dist/test.js
+++ dist/test.js
@@ -1,1 +1,11 @@
 "use strict";
+function fc(a) {
+    var result = [];
+    for (var _i = 0, a_1 = a; _i < a_1.length; _i++) {
+        var i = a_1[_i];
+        result.push(Number(i));
+    }
+    console.log(result);
+    return result;
+}
+fc(["1", 2, "3"]);
--- src/test.ts
+++ src/test.ts
@@ -1,1 +1,15 @@
-let arr: string = "h3";
+let 철수쌤: { subject: string } = { subject: "math" };
+let 영희쌤: { subject: string[] } = { subject: ["science", "english"] };
+let 민수쌤: { subject: string[] } = { subject: ["science", "art", "korean"] };
+
+function fc(x: { subject: string | string[] }): string {
+  if (typeof x.subject === "string") {
+    return x.subject;
+  } else if (Array.isArray(x.subject)) {
+    return x.subject[x.subject.length - 1];
+  }
+
+  return "";
+}
+
+fc(철수쌤);
